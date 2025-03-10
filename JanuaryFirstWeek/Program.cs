0cd4378dfe6b41420d126a18a40ba0047b0dbd98
İlker Yılmaz <ilkerylmaz.y@gmail.com>
2025-03-10 23:49:21 +0300

    January First Week Complated

---
+++ JanuaryFirstWeek/Program.cs
@@ -0,0 +1,15 @@
+﻿//C# Nedir?: C# microsoft tarafından geliştirilmiş bir programlama dilidir ve bir çok alanda kullanılabilir, Masaüstü, Web, Mobil, Oyun geliştirme gibi bir çok alanda kullanılabilir.
+//Ve güçlü kütüphane desteği mevcuttur.
+
+//Bir programın çalışması için gerekli temel bileşenler: 
+
+//CPU:Programın komutlarını işleyen ve programın çalışmasını sağlayan donanım birimidir.
+//RAM:Programın çalışması için gerekli olan verilerin geçici olarak saklandığı donanım birimidir.
+//Hard Disk:Programın çalışması için gerekli olan verilerin kalıcı olarak saklandığı donanım birimidir.
+//Ekran Kartı:Programın çalışması için gerekli olan verilerin ekrana gösterilmesini sağlayan donanım birimidir.
+//Anakart:Diğer donanım birimlerinin bir araya gelmesini sağlayan donanım birimidir.
+
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine(name);
