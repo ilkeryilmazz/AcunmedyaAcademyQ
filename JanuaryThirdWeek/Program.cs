951e846be99d67c4e2d1e84ba4050c95f00f9708
İlker Yılmaz <ilkerylmaz.y@gmail.com>
2025-03-11 00:39:15 +0300

    January Third Week Complated

---
+++ JanuaryThirdWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿//Koşullu ifadeler(if-else) ne işe yarar? Gerçek hayattan bir örnek verin
+
+// Koşullu ifadeler, programın belirli bir koşula bağlı olarak farklı işlemler yapmasını sağlar. Örneğin, ehliyet almak için belli yaş sınırları ve ekstra şartlar mevcut. Bu şartlar örnek sayılabilir.
+// Programlamada ise kullanıcı girişi, yetki kontrolü vb işlemler koşula bağlı yapılabilir.
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+    Console.WriteLine("Number is even");
+else
+    Console.WriteLine("Number is odd");
+
+
+if (number > 0)
+    Console.WriteLine("Number is positive.");
+else if (number < 0)
+    Console.WriteLine("Number is negative.");
+else
+    Console.WriteLine("Number is zero.");
